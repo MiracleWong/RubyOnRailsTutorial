8ec21f021dcdacaf5779c7ba6cbaa235df3a8940
MiracleWong <cfwr1991@126.com>
2018-01-26 11:04:55 +0800

    Add the Welcome message

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -4,8 +4,9 @@ class UsersController < ApplicationController
   end
   def create
     @user = User.new(user_params)
-    if
-    @user.save
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
     else
       render 'new'
     end
