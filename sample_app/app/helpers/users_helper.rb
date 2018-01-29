06e67392597c872de78660b79edf83b4e4d02664
MiracleWong <cfwr1991@126.com>
2018-01-29 23:58:46 +0800

    显示所有用户，生成随机用户数据

--- sample_app/app/helpers/users_helper.rb
+++ sample_app/app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   # 返回指定用户的Gravatar
-  def gravatar_for(user, options = { size: 100 })
+  def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size =  options[:size]
     gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
