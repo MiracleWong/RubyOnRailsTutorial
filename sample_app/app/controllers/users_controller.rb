edcf752da44280b1d13a3927569c029e111d59bf
MiracleWong <cfwr1991@126.com>
2018-01-29 23:09:04 +0800

    必须先登录才能进行修改

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -31,6 +32,13 @@ class UsersController < ApplicationController
   end
   private
 
+  # 前置过滤器，确保用户已经登录
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
